7122c21d789ed0903c1e3bccb4ae10754150c19c
ux-heesu <engine_hskim@uxplus.co.kr>
2023-11-24 23:30:58 +0900

    레이아웃 스타일 수정

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,51 +1,54 @@
 <template>
-    <header>
-        <div class="inner">
-            <h1 class="logo"><router-link to="/">Vue</router-link></h1>
-            <Nav />
-            <div class="user-box">
-                <router-link to="/member/login" class="btn btn-green btn-login">로그인</router-link>
-                <router-link to="/member/join" class="btn btn-green btn-join">회원가입</router-link>
-            </div>
-
-        </div>
-    </header>
+  <header>
+    <div class="inner">
+      <h1 class="logo"><router-link to="/">Vue</router-link></h1>
+      <Nav />
+      <div class="user-box">
+        <router-link to="/member/login" class="btn btn-green btn-login">로그인</router-link>
+        <router-link to="/member/join" class="btn btn-green btn-join">회원가입</router-link>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 import Nav from '@/components/common/Navigator.vue';
 export default {
-    name: 'Header',
-    components: {
-        Nav,
-    }
-}
+  name: 'Header',
+  components: {
+    Nav,
+  },
+};
 </script>
 
 <style scoped>
-header{
-    border-bottom: 1px solid #ddd;
+header {
+  border-bottom: 1px solid #ddd;
 }
-header .inner{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1rem;
+header .inner {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 5rem;
+  padding: 1rem;
 }
-.logo{
-    font-size: 3rem;
-    font-weight: 800;
-    color:#006900;
+.logo {
+  font-size: 3rem;
+  font-weight: 800;
+  color: #006900;
+}
+.user-box {
+  margin-left: auto;
 }
 .user-box > * {
-    padding: 0.8rem 1.2rem;
-    margin: 0 0.5rem;
-    font-size: 1.3rem;
-    font-weight: 600;
-    transition: all 0.3s;
-    -webkit-transition: all 0.3s;
-    -moz-transition: all 0.3s;
-    -ms-transition: all 0.3s;
-    -o-transition: all 0.3s;
+  padding: 0.8rem 1.2rem;
+  margin: 0 0.5rem;
+  font-size: 1.3rem;
+  font-weight: 600;
+  transition: all 0.3s;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -ms-transition: all 0.3s;
+  -o-transition: all 0.3s;
 }
 </style>
--- src/views/member/Login.vue
+++ src/views/member/Login.vue
@@ -1,32 +1,29 @@
 <template>
   <div class="content-wrap member">
-        <div class="inner">
-            <h2>로그인</h2>
-            <form class="form-style01">
-                <div class="form-box">
-                    <label class="form-tit" for="loginId">아이디</label>
-                    <input type="text" id="loginId" placeholder="아이디를 입력하세요">
-                </div>
-                <div class="form-box">
-                    <label class="form-tit" for="loginPw">비밀번호</label>
-                    <input type="password" id="loginPw" autocomplete="off" placeholder="비밀번호를 입력하세요">
-                </div>
-                <div class="btn-wrap">
-                    <button type="submit" class="btn btn-green-bg">로그인</button>
-                    <router-link to="/member/join" class="btn btn-green-bg">계정이 없나요?</router-link>
-                </div>
-                
-            </form>
+    <div class="inner">
+      <h2>로그인</h2>
+      <form class="form-style01">
+        <div class="form-box">
+          <label class="form-tit" for="loginId">아이디</label>
+          <input type="text" id="loginId" placeholder="아이디를 입력하세요" />
         </div>
+        <div class="form-box">
+          <label class="form-tit" for="loginPw">비밀번호</label>
+          <input type="password" id="loginPw" autocomplete="off" placeholder="비밀번호를 입력하세요" />
+        </div>
+        <div class="btn-wrap">
+          <button type="submit" class="btn btn-green-bg">로그인</button>
+          <router-link to="/member/join" class="btn btn-green">계정이 없나요?</router-link>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 // axios 예제
 export default {
-    name: 'Login',
-    components:{
-    }
-}
+  name: 'Login',
+  components: {},
+};
 </script>
-
